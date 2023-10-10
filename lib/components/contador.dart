a37d12e199cdd2ed4ad1b2c6b6c84f885fdc9e1d
matheus-liber <matheuslibergt@gmail.com>
2023-10-10 15:27:45 -0300

    Counter solution using setState

--- lib/components/contador.dart
+++ lib/components/contador.dart
@@ -1,27 +1,46 @@
 import 'package:flutter/material.dart';
 
-class Contador extends StatelessWidget {
-Contador({ Key? key }) : super(key: key);
+class Contador extends StatefulWidget {
+  Contador({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  State<Contador> createState() => _ContadorState();
+}
+
+class _ContadorState extends State<Contador> {
+  int valorContador = 0;
+
+  @override
+  Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
         InkWell(
           borderRadius: BorderRadius.circular(20),
           onTap: () {
-              
+            setState(() {
+              if (valorContador > 0) {
+                valorContador--;
+              }
+            });
           },
-          child: const Icon(Icons.remove_circle_outline, size: 20,),
+          child: const Icon(
+            Icons.remove_circle_outline,
+            size: 20,
+          ),
         ),
-        Text("0"),
+        Text(valorContador.toString()),
         InkWell(
           borderRadius: BorderRadius.circular(20),
           onTap: () {
-              
+            setState(() {
+              valorContador++;
+            });
           },
-          child: const Icon(Icons.add_circle_outline, size: 20,),
+          child: const Icon(
+            Icons.add_circle_outline,
+            size: 20,
+          ),
         ),
       ],
     );
